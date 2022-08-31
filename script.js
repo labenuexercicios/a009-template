b5f5373a99d120a075f5c2482427c60e0caa000d
guirozmesquita@gmail.com <guilhermesquita>
2022-08-31 08:38:54 -0400

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual é o seu nome?')
+let cor =  prompt('Qual sua cor favorita')
+
+console.log("A cor favorita de " + nome + " é " + cor)
+console.log(`A cor favorita de ${nome} é ${cor}`)

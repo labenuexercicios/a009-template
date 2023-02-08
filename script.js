5faa1e04b15826a9866b5b58a5545707ca18027e
henriquelbk <loubackhenrique123@gmail.com>
2023-02-08 20:56:06 -0300

    exercício de fixação

--- script.js
+++ script.js
@@ -17,8 +17,21 @@ const imprimeDiferente = function (nome, cor){
     console.log(nome.length)
 }
 
-const nome2 = (prompt('Seu nome:')).toUpperCase()
-const cor2 = prompt('Sua cor favorita:')
+//const nome2 = (prompt('Seu nome:')).toUpperCase()
+//const cor2 = prompt('Sua cor favorita:')
 
-imprimeDiferente(nome2, cor2)
+//imprimeDiferente(nome2, cor2)
 
+const cadastro = (nome, email) => {
+    const frase = `O email ${email} foi cadastrado com sucesso. Boas vindas, ${nome}.`
+    const novaFrase = frase.replaceAll('r','x')
+    console.log(frase)
+    console.log(novaFrase)
+    console.log(nome.length)
+    console.log(email.includes('@'))
+}
+
+const nome = prompt('Qual o seu nome?')
+const email = prompt('Qual o seu email?')
+
+cadastro(nome, email)

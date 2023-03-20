1525207e9dd4ce008ec33d35c658225a4f18502d
caroole01~ <caroline_voleck@hotmail.com>
2023-03-20 07:19:30 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,19 @@
+//Exercício 1
+/* let name = prompt("Digite seu nome");
+let favoritesOne = prompt("Digite a sua 1º comida favorita");
+let favoritesTwo = prompt("Digite a sua 2º comida favorita");
+let favoritesThree = prompt("Digite a sua 3º comida favorita");
+console.log("Meu nome é" + " "+ name + " " + "\nE minhas comidas favoritas são:" + "\n"+ "_" + favoritesOne + "\n" + "_" + favoritesTwo + "\n" + "_" + favoritesThree);
+ */
+//Exercício 2
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const novastring = minhaString.trim();
+console.log(novastring);
+console.log(novastring.length);
+
+//Exercício 3
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres:" + "BOAS VINDAS, mas não deixe o gato sair";
+const novaFrase = frase.replace()
+console.log(novaFrase.toLocaleUpperCase())

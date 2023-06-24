f8731d4edf5f240ddbbba76fe4019e626b0be0f7
Douglasluis508 <douglasluis508@gmail.com>
2023-06-24 19:48:21 -0300

    aula guiada de strings

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function imprimirPessoa (){
-    //digite a sua solução aqui
-}
+// const imprimirPessoa = ()=>{
+//     const nome =prompt('Digite seu nome');
+//     const corFavorita =prompt('Digite sua cor favorita')
+//     console.log(`A cor favorita de ${nome.toUpperCase()} é ${corFavorita}. A vida é louca.`)
+//     console.log(nome.length)
+//     console.log(nome.includes('a'))
+// };
+// imprimirPessoa()
 
-imprimirPessoa()
+
+const inserirDados = ()=>{
+    const nome =prompt('Digite seu nome');
+    const email =prompt('Digite seu e-mail');
+   console.log(`O e-mail ${email} foi cadastrado com sucesso. Boas vindas,
+        ${nome} o nome do usuário possui ${nome.length} letras.`)
+console.log(email.includes('@'))
+nome.replace('R,r', 'X,r'), email.replace('R,r', 'X,r')
+
+}
+   inserirDados()
+
